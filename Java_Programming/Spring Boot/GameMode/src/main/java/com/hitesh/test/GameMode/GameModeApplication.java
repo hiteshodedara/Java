8d3e2acc7a9528016d90f8aec5886025fc04c32d
Odedara Hitesh <hiteshodedara1010@gmail.com>
2023-08-24 12:55:41 +0530

    new program added

---
+++ Java_Programming/Spring Boot/GameMode/src/main/java/com/hitesh/test/GameMode/GameModeApplication.java
@@ -0,0 +1,13 @@
+package com.hitesh.test.GameMode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameModeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GameModeApplication.class, args);
+	}
+
+}
---
+++ Java_Programming/Spring Boot/GameMode/src/test/java/com/hitesh/test/GameMode/GameModeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hitesh.test.GameMode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameModeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
