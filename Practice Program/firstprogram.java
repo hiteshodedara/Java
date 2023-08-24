5424687347ed138c54d0f1b2344d444d22e6d84f
Odedara Hitesh <hiteshodedara1010@gmail.com>
2023-08-24 16:01:21 +0530

    new thing add

--- Practice Program/firstprogram.java
+++ Practice Program/firstprogram.java
@@ -1,24 +1,20 @@
-public class firstprogram{
+public class firstprogram {
 
-    public static void sum(){
-        int sal=18000;
-        int incre=20;
-        int year=0;
-        int cout=0;
+    public static void sum() {
+        int sal = 18000;
+        int incre = 20;
 
-        for(int i=0;i<=10;i++){
-            int ans=sal*incre/100;
-            sal=ans+sal;
-            
-
+        for (int i = 0; i <= 10; i++) {
+            int ans = sal * incre / 100;
+            sal = ans + sal;
             System.out.println(sal);
         }
-        
-    
+
     }
+
     public static void main(String[] args) {
-       
+
         sum();
-            
+
     }
 }
