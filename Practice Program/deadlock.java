89451cac9a7ff0337c11dcaab09312633abc8ca9
Odedara Hitesh <hiteshodedara1010@gmail.com>
2023-09-01 19:07:25 +0530

    add code in wait program and add new deadlock program in java

---
+++ Practice Program/deadlock.java
@@ -0,0 +1,45 @@
+class A{
+    public synchronized void d1(b b) throws InterruptedException{
+        System.out.println(" a method started...");
+        Thread.sleep(10000);
+        System.out.println("try to execute last method of A ...");
+        b.last();
+    }
+    public synchronized void last(){
+        System.out.println("i am last method of a class");
+    }
+}
+
+class B{
+    public synchronized void d2(a a) throws InterruptedException{
+        System.out.println(" b method started...");
+        Thread.sleep(10000);
+        System.out.println("try to execute last method of B ...");
+        a.last();
+    }
+    public synchronized void last(){
+        System.out.println("i am last method of b class");
+    }
+}
+
+class deadlock extends Thread{
+    A a1=new A();
+    B b1=new B();
+    public void maintom1() throws InterruptedException{
+        this.start();
+        a1.d1(b1);
+    }
+
+    public void run(){
+        try {
+            b1.d2(a1);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+    public static void main(String[] args) throws InterruptedException {
+        deadlock dead1=new deadlock();
+        dead1.maintom1();
+    }
+}
+
--- Practice Program/waitwithargument.java
+++ Practice Program/waitwithargument.java
@@ -1,5 +1,41 @@
-class waitwithargument{
-    public static void main(String[] args) {
+class testwaitargument extends Thread{
+    // testwaitargument(){}
+
+    int total=0;
+    public void run(){
+synchronized(this){
+        System.out.println("loop started...");
+        for (int i = 1; i <=100; i++) {
+            total=total+i;
+        }
+        System.out.println(total);
+    }
+    try {
+        Thread.sleep(10000);
+    } catch (InterruptedException e) {
         
+        e.printStackTrace();
+    }
+        System.out.println("loop is complated...");
+
+    }
+}
+
+public class waitwithargument {
+    public static void main(String[] args) throws InterruptedException  {
+        System.out.println("main thread started...");
+        
+        testwaitargument twa1=new testwaitargument();
+        
+        System.out.println("Starting new thread...");
+        twa1.start();
+        synchronized(twa1){
+        
+        System.out.println("starting wait method");
+        twa1.wait(5000);            }
+        System.out.println("total is = "+twa1.total);
+        
+        System.out.println("main thread is completed...");
+
     }
 }
