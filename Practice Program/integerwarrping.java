20719eb70786386dc2bab517b0b83755201d90d6
hiteshodedara <hiteshodedara1010@gmail.com>
2023-10-05 19:00:13 +0530

    new java program add in list

---
+++ Practice Program/integerwarrping.java
@@ -0,0 +1,7 @@
+public class integerwarrping {
+    public static void main(String[] args) {
+        String s1 =Integer.toString(15,2);
+        String s2 =Integer.toBinaryString(15);
+        System.out.println(s2);
+    }
+}
