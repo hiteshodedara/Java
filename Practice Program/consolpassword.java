ff65f4754443a88375fc0af18d04a8475b2409c8
hiteshodedara <hiteshodedara1010@gmail.com>
2023-10-06 12:46:28 +0530

    new code added and some bug fix

--- Practice Program/consolpassword.java
+++ Practice Program/consolpassword.java
@@ -1,19 +1,36 @@
 import java.io.Console;
-public class consolpassword {
-	public static void main(String[] args) {
+public class consolpassword{
+	
+public static void login() {
 		Console cons;
+		String username;
+		String id=null;
+		char[] password = null;
 		if ((cons = System.console()) != null) {
-			char[] pass_ward = null;
 			try {
-				pass_ward = cons.readPassword("Input your Password:");
-				System.out.println("Your password was: " + new String(pass_ward));
+				id = cons.readLine("Input your ID:");
+				username = cons.readLine("Input your Username:");
+				password = cons.readPassword("Input your Password:");
+				System.out.println("Your ID was: " + id);
+				System.out.println("Your username was: " + username);
+				System.out.println("Your password was: " + new String(password));
 			} finally {
-				if (pass_ward != null) {
-					java.util.Arrays.fill(pass_ward, ' ');
+				if (password != null) {
+					java.util.Arrays.fill(password, ' ');
 				}
 			}
 		} else {
 			throw new RuntimeException("Can't get password...No console");
 		}
 	}
-}
+	public static void main(String[] args) {
+		
+		login();
+	}
+
+} 
+	
+	
+
+	
+
