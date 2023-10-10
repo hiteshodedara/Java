216939d4306083767e926067597e91be01adaa25
hiteshodedara <hiteshodedara1010@gmail.com>
2023-10-10 12:13:19 +0530

    add new thing

--- Practice Program/warrperdemo3.java
+++ Practice Program/warrperdemo3.java
@@ -1,16 +1,16 @@
 public class warrperdemo3 {
     public static void main(String[] args) {
-        // Integer s1=new Integer(10);
-        // Integer s2=new Integer(10);
-        // System.out.println(s1==s2);
+        Integer s1=new Integer(10);
+        Integer s2=new Integer(10);
+        System.out.println(s1==s2);
 
-        // Integer s3=10;
-        // Integer s4=10;
-        // System.out.println(s3==s4);
+        Integer s3=10;
+        Integer s4=10;
+        System.out.println(s3==s4);
 
-        // Integer s5=Integer.valueOf(10);
-        // Integer s6=Integer.valueOf(10);
-        // System.out.println(s5==s6);
+        Integer s5=Integer.valueOf(10);
+        Integer s6=Integer.valueOf(10);
+        System.out.println(s5==s6);
 
         Integer s1=127;
         Integer s2=127;
